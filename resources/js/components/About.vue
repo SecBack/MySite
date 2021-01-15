cc457a37622e98071c6898c0cbf3d8918ae4b71d
SecBack <albert3257@live.dk>
2021-01-16 00:03:16 +0100

    add project to git

---
+++ resources/js/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+<b-container class="bv-example-row">
+  <b-row>
+    <b-col>
+      <b-row>
+        Who am i
+      </b-row>
+      <b-row>
+        <b-img src="https://picsum.photos/300/300"></b-img>
+      </b-row>
+    </b-col>
+    <b-col>text about me here</b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
---
+++ resources/js/views/app.vue
@@ -0,0 +1,53 @@
+<template>
+<div id="app">
+  <div style="height: 100%;">
+    <vue-particles
+      color="#ffffff"
+      :particleSize="1"
+      :linesWidth="2"
+      :linesDistance="150"
+      :lineOpacity="1000"
+      :moveSpeed="6"
+      :particlesNumber="80"
+    ></vue-particles>
+  </div>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+
+  <About></About>
+
+  <Footer></Footer>
+</div>
+</template>
+
+<script>
+import About from '../components/About.vue';
+import Footer from '../components/Footer';
+
+const default_layout = "default";
+
+export default {
+  components: {
+    About,
+    Footer,
+  },
+  computed: {},
+  data() {
+    return {
+      message: "hej"
+    }
+  }
+};
+</script>
