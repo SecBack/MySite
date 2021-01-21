8a39257605fd5c120cf71ce509b5260a70b25701
SecBack <albert3257@live.dk>
2021-01-21 14:19:18 +0100

    added success method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,44 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Create a JSON success response.
+     *
+     * @param mixed[] $data
+     * @param string $message
+     * @param int $status
+     * @param string[] $headers
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function success(array $data = [], $message = 'Success', $status = 200, array $headers = [])
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+            'code' => $status,
+        ], $status, $headers);
+    }
+
+    /**
+     * Create a JSON error response.
+     *
+     * @param mixed[] $data
+     * @param string $message
+     * @param int $status
+     * @param string[] $headers
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function error(array $data = [], $message = 'Error', $status = 422, array $headers = [])
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $data,
+            'code' => $status,
+        ], $status, $headers);
+    }
 }
